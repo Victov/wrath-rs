c08ff21af02c691b4e4f65d0704c19657f9f9133
victov <victor.veldstra@gmail.com>
2020-09-02 15:36:31 +0200

    initial commit

---
+++ migrations/20200830073445_setup.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `accounts` 
+(
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Identifier',
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `sha_pass_hash` varchar(40) NOT NULL DEFAULT '',
+  `sessionkey` varchar(80) NOT NULL DEFAULT '',
+  `v` varchar(64) NOT NULL DEFAULT '',
+  `s` varchar(64) NOT NULL DEFAULT '',
+  `token_key` varchar(100) NOT NULL DEFAULT '',
+  `banned` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
---
+++ src/constants.rs
@@ -0,0 +1,30 @@
+#[allow(dead_code)]
+pub enum AuthResult
+{
+    Success = 0x00,
+    FailBanned = 0x03,
+    FailUnknownAccount = 0x04,
+    FailIncorrectPassword = 0x05,
+    FailAlreadyOnline = 0x06,
+    FailNoGameTime = 0x07,
+    FailDatabaseBusy = 0x08,
+    FailVersionInvalid = 0x09,
+    FailVersionUpdateAvailable = 0x0A,
+    FailInvalidServer = 0x0B,
+    FailSuspended = 0x0C,
+    FailNoAccess = 0x0D,
+    SuccessSurvey = 0x0E,
+    FailParentalControl = 0x0F,
+    FailLockedEnforced = 0x10,
+    FailTrialEnded = 0x11,
+    FailUseBattlenet = 0x12,
+    FailAntiIndulgence = 0x13,
+    FailExpired = 0x14,
+    FailNoGameAccount = 0x15,
+    FailChargeBack = 0x16,
+    FailInternetGameRoomWithoutBattleNet = 0x17,
+    FailGameAccountLocked = 0x18,
+    FailUnlockableLock = 0x19,
+    FailConversionRequired = 0x20,
+    FailDisconnected = 0xFF
+}
