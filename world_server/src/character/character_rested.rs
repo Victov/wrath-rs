864127c0987a817d37ac83abab7e088619a5117e
victov <victor.veldstra@gmail.com>
2022-05-08 20:56:05 +0200

    Add explanations of unfinished resting state in order to wrap up areatriggers

--- world_server/src/character/character_rested.rs
+++ world_server/src/character/character_rested.rs
@@ -10,7 +10,7 @@ pub(super) enum RestedState {
 #[derive(PartialEq, Debug)]
 #[allow(dead_code)]
 pub enum RestedLocation {
-    City,
+    City, //Unused until server-side maps are implemented and the server can know when a character enters a city.
     Inn,
 }
 
@@ -24,6 +24,10 @@ impl super::Character {
     }
 
     pub fn is_in_rested_area(&self) -> bool {
+        /* Right now, after entering an inn once, the character will stay rested for the rest of
+         * the session. The server needs to check area changes on tick, for which server-side maps
+         * need to be implemented. Until that is done, un-resting will not work */
+
         match &self.rested_state {
             RestedState::NotRested => false,
             RestedState::Rested(location) => match location {
