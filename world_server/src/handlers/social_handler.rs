ee553dfa25ec78e28fc358e1ce8eb7bf709ebb7a
victov <victor.veldstra@gmail.com>
2022-10-16 11:05:42 +0200

    handle CMSG_SET_SELECTION

--- world_server/src/handlers/social_handler.rs
+++ world_server/src/handlers/social_handler.rs
@@ -1,8 +1,8 @@
-use crate::character::*;
 use crate::packet::ServerMessageExt;
 use crate::prelude::*;
+use crate::{character::*, client_manager::ClientManager};
 
-use wow_world_messages::wrath::{RelationType, SMSG_CONTACT_LIST};
+use wow_world_messages::wrath::{RelationType, CMSG_SET_SELECTION, SMSG_CONTACT_LIST};
 
 pub async fn send_contact_list(character: &Character, relation_mask: RelationType) -> Result<()> {
     SMSG_CONTACT_LIST {
@@ -11,4 +11,14 @@ pub async fn send_contact_list(character: &Character, relation_mask: RelationTyp
     }
     .astd_send_to_character(character)
     .await
+}
+
+pub async fn handle_csmg_set_selection(client_manager: &ClientManager, client_id: u64, packet: &CMSG_SET_SELECTION) -> Result<()> {
+    let client = client_manager.get_authenticated_client(client_id).await?;
+    let character_lock = client.get_active_character().await?;
+
+    let mut character = character_lock.write().await;
+    let selection = if packet.target.is_zero() { None } else { Some(packet.target) };
+    character.set_selection(selection);
+    Ok(())
 }
