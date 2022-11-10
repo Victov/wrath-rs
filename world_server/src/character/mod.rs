ae55de805c7d1a9a65321a9600695d4bdb846ae1
victov <victor.veldstra@gmail.com>
2022-11-10 22:06:11 +0100

    Clippy fixes

--- world_server/src/character/mod.rs
+++ world_server/src/character/mod.rs
@@ -197,7 +197,7 @@ impl Character {
     }
 
     pub fn set_visible_actionbar_mask(&mut self, action_bars: u8) {
-        let (a, b, c, d) = self.gameplay_data.player_FIELD_BYTES().unwrap_or_default();
+        let (a, b, _, d) = self.gameplay_data.player_FIELD_BYTES().unwrap_or_default();
         //action_bars is a flags, no extra actionbars = 0, all bars (2 above default bar, 2 side
         //bars) is 15 when they are set visible in the Interface settings menu
         self.gameplay_data.set_player_FIELD_BYTES(a, b, action_bars, d);
