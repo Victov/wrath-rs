770a79abfeb5dd7344cda94873ecba91798b7dc5
Dan Costinas <danscostinas@gmail.com>
2023-07-16 13:17:36 +0300

    Make ParseError report work with new wow_messages

--- world_server/src/client.rs
+++ world_server/src/client.rs
@@ -126,11 +126,10 @@ impl Client {
                     payload: Box::new(op),
                 })?;
             } else if let Err(e) = opcode {
-                //TOOD: Make this work again
-                // if let ExpectedOpcodeError::Parse(ParseError::Io(_)) = e {
-                //     error!("IO error during parsing, there is no recovery from this, disconnect client");
-                //     break;
-                // }
+                if let ExpectedOpcodeError::Parse(_) = e {
+                    error!("IO error during parsing, there is no recovery from this, disconnect client");
+                    break;
+                }
                 warn!("Error in opcode: {}.", e);
             }
         }
